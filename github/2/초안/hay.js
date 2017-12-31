bce5c0210c2590de4c1911b0de4e1edaaa640d01
namadjw12 <jwsuh0703@gmail.com>
2017-12-31 13:50:49 +0900

    add assignment

---
+++ github/2/초안/hay.js
@@ -0,0 +1,17 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function() {
+    $(this).removeClass("hover");
+  }
+);
+<div class="userEL1350852">
+ <div class="container">
+  <div class="row">
+
+   <div class="col-xs-12 col-sm-12 col-md-12">
+    <h2 class="down" data-edit="true" data-selector="h2.down" >하아 씨발</h2>
+   </div>
+
+  </div>
+ </div>
+</div>
